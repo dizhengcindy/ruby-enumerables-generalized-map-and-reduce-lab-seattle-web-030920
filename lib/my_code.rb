33cbfbf42e3a77d0c5010db33aa32bcc4a59ec46
Di Zheng <dizhengcindy@gmail.com>
2020-02-26 00:56:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,18 +11,17 @@ end
 
 def reduce(array, starting_value = nil)
   if starting_value
-    num = starting_value
+    sum = starting_value
     i = 0
   else
-    num =array[0]
+    sum =array[0]
     i = 1 
   end
   
   while i < array.length do 
-  num = yield(num, array[i])
-
+  sum = yield(sum, array[i])
   i += 1
   end
-  num 
+  sum 
 end
 
