5f6999b357cce631eed361cb879748b32edac86b
Di Zheng <dizhengcindy@gmail.com>
2020-02-26 00:20:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+def map(array)
+new=[]
+i = 0 
+while i < array.length do
+  new.push(yield(array[i]))
+  i+=1
+end 
+new
+end
+
+def reduce(array, starting_value = nil)
+  if starting_value
+    sum = starting_value
+  else
+    sum = 0 
+  end
+  
+  i = 0 
+  while i < array.length do 
+  sum += array[i]
+  i += 1
+  end
+  sum 
+end
